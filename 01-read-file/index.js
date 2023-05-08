4756ce26af13b4a5236e375246212dec5ddd9647
Ivan Lebedev <vaanya@ya.ru>
2023-05-08 23:43:04 +0300

    feat: first task solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+// const text = require('./text.txt');
+
+let fs = require('fs');
+
+fs.readFile('01-read-file/text.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
